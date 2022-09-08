a8cfd72fb89beda33089b0edc5c897a03e1fcce0
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-09-08 19:38:21 -0300

    req05-fix: retirei as props e states que havia colocado no commit anterior

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import SearchProducts from './components/SearchProducts';
 
 export default class App extends Component {
-  state = {
-    queryInput: '',
-  };
-
   render() {
-    const { queryInput } = this.state;
     return (
       <BrowserRouter>
         <Switch>
           {/* alterei a sintaxe do route para eu conseguir passar props para o SearchProducts */}
           <Route path="/">
-            <SearchProducts
-              queryInput={ queryInput }
-            />
+            <SearchProducts />
           </Route>
         </Switch>
       </BrowserRouter>
