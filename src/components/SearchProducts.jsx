0b82690f156b6797818eeb4e3d927edc5549d746
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-09-08 18:21:45 -0300

    req02-feat: coloquei a renderização condicional da mensagem do termo de pesquisa vazio

--- src/components/SearchProducts.jsx
+++ src/components/SearchProducts.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class SearchProducts extends Component {
   render() {
+    const { queryInput } = this.props;
     return (
       <div>
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
+        { queryInput === '' && (
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+        )}
       </div>
     );
   }
 }
+
+SearchProducts.propTypes = {
+  queryInput: PropTypes.string,
+}.isRequired;
