41fbbdd25a9367c2ae5c2f787aa9b826dc4def6b
Bruno Modesto <contato.obruniin@gmail.com>
2022-09-08 19:20:36 -0300

    finalizando req03

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import SearchProducts from './components/SearchProducts';
+import ShoppingCart from './pages/ShoppingCart';
 
 export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
+          <Route path="/carrinho" component={ ShoppingCart } />
           <Route path="/" component={ SearchProducts } />
         </Switch>
       </BrowserRouter>
