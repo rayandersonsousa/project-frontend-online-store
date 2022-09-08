255e17e4dced16ffbd4e3e697683b7eb1b44d8c0
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-09-08 19:48:00 -0300

    req05-feat: alterei o SearchProducts para que tivesse o input e o button e o map dos resultados no ProductCard

--- src/components/SearchProducts.jsx
+++ src/components/SearchProducts.jsx
@@ -1,22 +1,65 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { getProductsFromCategoryAndQuery } from '../services/api';
+import ProductCard from './ProductCard';
 
 export default class SearchProducts extends Component {
+  state = {
+    queryInput: '',
+    queryResults: [],
+    notFound: false,
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  };
+
+  handleClick = async () => {
+    const { queryInput } = this.state;
+    const data = await getProductsFromCategoryAndQuery('', queryInput);
+    this.setState({ queryResults: data.results }, () => {
+      this.setState({ notFound: data.results.length === 0 });
+    });
+  };
+
   render() {
-    const { queryInput } = this.props;
+    const { queryInput, queryResults, notFound } = this.state;
+
     return (
       <div>
+        <input
+          name="queryInput"
+          data-testid="query-input"
+          id=""
+          type="text"
+          value={ queryInput }
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="query-button"
+          type="button"
+          onClick={ this.handleClick }
+        >
+          Pesquisar
+        </button>
         {/* adicionei a renderização condicional da mensagem termo vazio conforme a prop queryInput */}
         { queryInput === '' && (
           <p data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
           </p>
         )}
+        {notFound ? (
+          <p>
+            Nenhum produto foi encontrado
+          </p>
+        ) : queryResults.map((e) => (
+          <ProductCard
+            key={ e.id }
+            dataTestId="product"
+            title={ e.title }
+            price={ e.price }
+            thumbnail={ e.thumbnail }
+          />))}
       </div>
     );
   }
 }
-
-SearchProducts.propTypes = {
-  queryInput: PropTypes.string,
-}.isRequired;
