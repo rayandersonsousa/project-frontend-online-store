abc794971966c64c0ec7dd2ca0ec973be5476b5d
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-09-08 18:20:41 -0300

    req05-feat: adicionei o state queryInput ao App.js e alterei a sintaxe do route / para poder passar props

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import SearchProducts from './components/SearchProducts';
 
 export default class App extends Component {
+  state = {
+    queryInput: '',
+  };
+
   render() {
+    const { queryInput } = this.state;
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={ SearchProducts } />
+          {/* alterei a sintaxe do route para eu conseguir passar props para o SearchProducts */}
+          <Route path="/">
+            <SearchProducts
+              queryInput={ queryInput }
+            />
+          </Route>
         </Switch>
       </BrowserRouter>
     );
