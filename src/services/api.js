d1a924461bff774367011f8cc67717c50294d04b
arthur-debiasi <arthurdebiasi@hotmail.com>
2022-09-09 16:42:22 -0300

    req06-feat: cria a função getProductById

--- src/services/api.js
+++ src/services/api.js
@@ -18,7 +18,12 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   }
 }
 
-export async function getProductById() {
-  // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
-  // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
+export async function getProductById(categoryId) {
+  const URL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  try {
+    const res = await fetch(URL);
+    return res.json();
+  } catch (error) {
+    return error;
+  }
 }
