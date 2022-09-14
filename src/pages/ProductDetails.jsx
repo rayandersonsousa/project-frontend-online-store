5ebcefedf44d689f95f5912eb26c41f18bc949ad
Arthur Debiasi <106253453+arthur-debiasi@users.noreply.github.com>
2022-09-14 14:47:08 -0300

    Revert "Main group 23 req12"

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -2,91 +2,22 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { getProductDetailsById } from '../services/api';
 import ShoppingCartButton from '../components/ShoppingCartButton';
-import ProductDetailsEvaluationForm from '../components/ProductDetailsEvaluationForm';
-import ProductDetailsEvaluationCard from '../components/ProductDetailsEvaluationCard';
 
 export default class ProductDetails extends Component {
   state = {
     title: '',
     thumbnail: '',
     price: '',
-    emailInput: '',
-    evaluationText: '',
-    rate: 0,
-    evaluationList: [],
-    isInvalidFields: false,
   };
 
   async componentDidMount() {
     const { match: { params: { id } } } = this.props;
-    const evaluationList = JSON.parse(localStorage.getItem(id)) || [];
     const { title, thumbnail, price } = await getProductDetailsById(id);
-    this.setState({ title, thumbnail, price, evaluationList });
+    this.setState({ title, thumbnail, price });
   }
 
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({
-      [name]: name === 'rate' ? Number(value) : value,
-    });
-  };
-
-  handleValidation = () => {
-    const { emailInput, rate } = this.state;
-    const regexp = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i; // https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
-    const trueEmail = emailInput.match(regexp);
-    console.log(emailInput, rate);
-    return trueEmail && (rate !== 0);
-  };
-
-  handleClick = () => {
-    const validation = this.handleValidation();
-    const {
-      emailInput,
-      evaluationText,
-      rate,
-      evaluationList,
-      // isInvalidFields,
-    } = this.state;
-    const { match: { params: { id } } } = this.props;
-    console.log(validation);
-    if (validation) {
-      this.setState({
-        evaluationList: [...evaluationList, {
-          email: emailInput,
-          text: evaluationText,
-          rating: rate,
-        }],
-      }, () => {
-        this.setState({
-          emailInput: '',
-          evaluationText: '',
-          rate: 0,
-          isInvalidFields: false,
-        });
-        localStorage.setItem(id, JSON.stringify([...evaluationList, {
-          email: emailInput,
-          text: evaluationText,
-          rating: rate,
-        }]));
-      });
-    } else {
-      this.setState({ isInvalidFields: true });
-    }
-  };
-
   render() {
-    const {
-      title,
-      thumbnail,
-      price,
-      emailInput,
-      evaluationText,
-      rate,
-      isInvalidFields,
-      evaluationList,
-    } = this.state;
-
-    const { match: { params: { id } }, handleCartButton } = this.props;
+    const { title, thumbnail, price } = this.state;
     return (
       <div>
         <span data-testid="product-detail-name">
@@ -98,32 +29,7 @@ export default class ProductDetails extends Component {
         </span>
         <br />
         <img src={ thumbnail } alt={ title } data-testid="product-detail-image" />
-        <button
-          type="button"
-          data-testid="product-detail-add-to-cart"
-          onClick={ () => handleCartButton(id) }
-        >
-          Adicionar ao carrinho
-        </button>
         <ShoppingCartButton />
-        <ProductDetailsEvaluationForm
-          handleChange={ this.handleChange }
-          emailInput={ emailInput }
-          evaluationText={ evaluationText }
-          rate={ rate }
-          isInvalidFields={ isInvalidFields }
-          handleClick={ this.handleClick }
-        />
-        <ul>
-          {evaluationList.map(({ email, text, rating }) => (
-            <ProductDetailsEvaluationCard
-              key={ text }
-              email={ email }
-              text={ text }
-              rating={ rating }
-            />
-          ))}
-        </ul>
       </div>
     );
   }
@@ -133,4 +39,4 @@ ProductDetails.propTypes = {
   match: PropTypes.object,
   params: PropTypes.object,
   id: PropTypes.string,
-}.isRequired;
+}.isRequeired;
